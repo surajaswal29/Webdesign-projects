ff03692f52db0abcb5e40f9d6d8393c60457b5e9
Suraj Aswal <87890258+surajaswal-dev@users.noreply.github.com>
2022-06-24 00:37:19 +0530

    Add files via upload

--- hnbgu-council-clubs/js/main.js
+++ hnbgu-council-clubs/js/main.js
@@ -2,11 +2,19 @@
 // Author: Suraj Aswal
 const downClick = document.querySelectorAll("#down-tab"); //getting all click button element
 const hnbguClub = document.querySelectorAll("#hnbgu-clubs"); //getting all dropdown element
+const hamMenuBtn = document.querySelector("#ham-default");
+const hamMenuDropdown = document.querySelector("#hnbgu-nav-links");
 // console.log(downClick);
-
+console.log(hamMenuBtn);
 // performing event for every node
-for (let i = 0; i <= downClick.length; i++) {
-  downClick[i].addEventListener("click", function () {
+for (let i = 0; i < downClick.length; i++) {
+  const ele = downClick[i];
+  ele.addEventListener("click", function () {
     hnbguClub[i].classList.toggle("hnbgu-clubs-hide");
   });
 }
+console.log("eroooor😂😂😂");
+hamMenuBtn.addEventListener("click", function () {
+  console.log("clicked Menu");
+  hamMenuDropdown.classList.toggle("hide-navbar");
+});
